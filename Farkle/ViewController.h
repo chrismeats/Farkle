494ae4839784022fb28b0992ff2a68afc4b8c09c
Chris Meats <cmeats@etccomputerland.com>
2014-07-30 13:10:13 -0500

    first commit

---
+++ Farkle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by ETC ComputerLand on 7/30/14.
+//  Copyright (c) 2014 cmeats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
